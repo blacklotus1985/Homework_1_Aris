061ebae467a8f295c6fda6ab3ef960f5c766ddb1
Unknown <alexander.zeisberg85@gmail.com>
2019-10-13 14:49:02 +0200

    commits

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Homework_1_Aris_
+all raw_inputs and prints have been modified to be worked on python 3.7
---
+++ arithmetic_operators.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    # print sum, substracion and multiplication of two numbers
+    a = int(input())
+    b = int(input())
+    print (a+b)
+    print (a-b)
+    print (a*b)
